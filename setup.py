c2f665721bdae7ce04445cdcf665962c1197e2c8
sparkaar <sparkaar@hu-berlin.de>
2024-05-30 13:10:01 +0200

    Tried to fix setup missing files

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 setup(name='CASCADE',
-      entry_points={
+    entry_points={
               'console_scripts': [
                   'CASCADE = cascade.CLI:main',
               ],              
           },
-        install_requires=[
+    install_requires=[
             'tqdm',
             'openai',
             'docker',
