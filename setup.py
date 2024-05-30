342a736d5a028db0f1379e36c0780f2a937eb6b2
sparkaar <sparkaar@hu-berlin.de>
2024-05-30 13:17:18 +0200

    Tried to fix setup missing files - Electric Boogaloo

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
 setup(name='CASCADE',
-    entry_points={
-              'console_scripts': [
-                  'CASCADE = cascade.CLI:main',
-              ],              
-          },
-    install_requires=[
-            'tqdm',
-            'openai',
-            'docker',
-            'tiktoken'
-        ]
+      entry_points={
+          'console_scripts': [
+              'CASCADE = cascade.CLI:main',
+          ],
+      },
+      install_requires=[
+          'tqdm',
+          'openai',
+          'docker',
+          'tiktoken'
+      ],
+      include_package_data=True
 )
