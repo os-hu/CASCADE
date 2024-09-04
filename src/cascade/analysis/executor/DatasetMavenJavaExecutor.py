22620ed7242474ea9350682114d95ccabe0ac1e1
TobiasKiecker <tobias.kiecker@hu-berlin.de>
2024-09-04 16:36:27 +0200

    preparations  for dataset benchmark

--- src/cascade/analysis/executor/DatasetMavenJavaExecutor.py
+++ src/cascade/analysis/executor/DatasetMavenJavaExecutor.py
@@ -10,8 +10,25 @@ class DatasetMavenJavaExecutor(MavenJavaExecutor):
 
     def execute(self, code: str, tests: str, context: dict, output_path) -> (succeeded, failed, errored):
 
-        # buidl looop
+        # TODO? get mven to display all possbile paramters that migth be used here
 
-        super().execute(code, tests, context, output_path)
+        for arg in self.args:
+            # try to run maven with the arg
+            print("Trying arg: ", arg)
+            self.builder = MavenBuilder(
+                new_image_name="maven_modified",
+                maven_args=arg,
+                set_up_maven_command="test",
+                set_up_maven_args=arg,
+                image="maven"
+            )
 
-        # if it wokred set args to the working arg
+            result = super().execute(code, tests, context, output_path)
+
+            if not result == ([],[],[]):
+                # if it worked set args to the working arg
+                self.args = [arg]
+                print("Working arg: ", arg)
+                return result
+
+        return [],[],[]
