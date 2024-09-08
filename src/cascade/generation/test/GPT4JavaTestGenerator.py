11a7376b719ca18c5eb98b141c477b1263ac9ff5
TobiasKiecker <tobias.kiecker@hu-berlin.de>
2024-09-08 13:04:59 +0200

    fixed error...

--- src/cascade/generation/test/GPT4JavaTestGenerator.py
+++ src/cascade/generation/test/GPT4JavaTestGenerator.py
@@ -58,8 +58,8 @@ class GPT4JavaTestGenerator(Generator):
     def build_tests(self, context, primer=""):
         packg_declaration = f"package {context['test_package']};\n\n"
         imports = "".join(context["test_imports"]) + "\n"
-        # imports += "import org.junit.Test;\n"
-        # test this later
+        imports += "import org.junit;\n"
+
         classdefinition = "public class " + context["test_file_path"].split("/")[-1].split(".")[0] + "{"
         name = str(context["signature"]["name"])
         func_definition = "\n    @Test\n    public void test" + name[0].upper() + name[1:] + "1(){"
