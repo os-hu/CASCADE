89b1ead6867980e260f7399609e4f276c56b37af
TobiasKiecker <tobias.kiecker@hu-berlin.de>
2024-09-16 17:15:12 +0200

    Fixed emptyAnalysis.py

--- src/cascade/analysis/emptyAnalysis.py
+++ src/cascade/analysis/emptyAnalysis.py
@@ -14,17 +14,16 @@ class emptyAnalysis(Analysis):
     TODO
     """
     def __init__(self, generator: Generation, executor: Execution, visualizer: Visualization):
-        self.generator = generator
-        self.executor = executor
-        self.visualizer = visualizer
+        super().__init__(generator, executor, visualizer)
         self.provided = Requirements(Requirements.Kind.PROVIDED, name="Analysis-Provided")
         self.provided.add_requirement("all", Requirements.Level.MANDATORY)
         self.extraction_requirements = Requirements(Requirements.Kind.EXPECTED, name="Analysis-Extraction-Expected")
 
 
-    def analyse(self, data, output_path):
+    def analyse(self, data, input_path, output_path):
         """
         This is the main analysis method.
+        :param **kwargs:
         :return:
         """
         
