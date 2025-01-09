48369ad911376183f6f946610e5cce41aefbdd93
TobiasKiecker <tobias.kiecker@hu-berlin.de>
2025-01-09 18:57:10 +0100

    added new multi step classes

---
+++ src/cascade/analysis/MultiStepDatasetAnalysis.py
@@ -0,0 +1,354 @@
+import os
+import re
+import shutil
+import tempfile
+
+from cascade.analysis.Analysis import Analysis
+from cascade.analysis.executor.Execution import Execution
+from cascade.analysis.visualizer.Visualization import Visualization
+from cascade.extraction.JavaExtraction import JavaExtraction
+
+from cascade.generation.Generation import Generation
+from cascade.utils.Utils import load_json_from_path, log, save_dicts_list_to_json
+
+from cascade.utils.DockerizedWrapper import DockerizedWrapper
+import xml.etree.ElementTree as ET
+
+class MultiStepDatasetAnalysis(Analysis):
+    def __init__(self, generator: Generation, executor: Execution, visualizer: Visualization, regenerate=False, reexecute=False, image="maven" , debug=0, step_size=1):
+        super().__init__(generator, executor, visualizer)
+        self.reexecute = reexecute or regenerate
+        self.step_size = step_size
+        self.regenerate = regenerate
+        self.debug = debug
+        self.visualizer.logger = "tqdm"
+        self.image = image
+
+    def extract_junit_version(self, input_path, output_path):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            try:
+                shutil.copytree(input_path, temp_dir, dirs_exist_ok=True)
+
+            except Exception as e:
+                print("could not copy root path")
+                print(e)
+
+            dock_ex = DockerizedWrapper(debug=0)
+
+            # get the effective pom file, which usually contains the used junit version
+            dock_context = {
+                "image" : self.image,
+                "directory" : temp_dir,
+                "command" : "mvn help:effective-pom -Doutput=effective-pom.xml",
+                "path" : "/root/effective-pom.xml"
+            }
+
+            dock_ex.execute(dock_context, output_path, copy=True)
+
+        try:
+            tree = ET.parse(os.path.join(output_path, "effective-pom.xml"))
+            root = tree.getroot()
+
+            # Define namespaces, if they exist in your pom.xml
+            namespaces = {'m': 'http://maven.apache.org/POM/4.0.0'}  # Default Maven namespace
+
+            # Initialize variables for return values
+            junit_version = "JUnit dependency not found"
+            source_dir = "Source directory not specified"
+            test_source_dir = "Test source directory not specified"
+
+            # Search for JUnit dependency
+            for dependency in root.findall(".//m:dependency", namespaces):
+                group_id = dependency.find("m:groupId", namespaces)
+                artifact_id = dependency.find("m:artifactId", namespaces)
+                if group_id is not None and artifact_id is not None:
+                    if group_id.text == "junit" and artifact_id.text == "junit":
+                        version = dependency.find("m:version", namespaces)
+                        if version is not None:
+                            junit_version = version.text
+                        else:
+                            junit_version = "Version not specified"
+
+            # Extract source and test source directories
+            build = root.find(".//m:build", namespaces)
+            if build is not None:
+                source_directory = build.find("m:sourceDirectory", namespaces)
+                test_source_directory = build.find("m:testSourceDirectory", namespaces)
+
+                if source_directory is not None:
+                    source_dir = source_directory.text
+                if test_source_directory is not None:
+                    test_source_dir = test_source_directory.text
+
+            # Return a 3-tuple with JUnit version, source directory, and test source directory
+            return junit_version, source_dir, test_source_dir
+
+        except ET.ParseError as e:
+            return f"Error parsing pom.xml: {e}", None, None
+
+    def analyse(self, data: list, input_path, output_path):
+        """
+        this is the specific analysis for the dataset benchmark. it only executes level 2 and 3 of a normal tree analysis.
+        it does not visualize anything. it does however safe the results in a file called result_CASCADE.txt
+
+        :param data:
+        :param input_path:
+        :param output_path:
+        """
+
+        #for interesting statisitcs:
+        amount_errored_testclasses = 0
+        amount_generated_testclasses = 0
+
+        output = ""
+
+        ana_path = os.path.join(output_path, "analyzed.json")
+
+        # load data for this specific run.
+        data = load_json_from_path(ana_path)
+
+        d = data[0]
+
+        # extract functions
+        extractor = JavaExtraction()
+        extractor.extract(input_path, output_path)
+
+        if "junit_version" not in d:    #remove this if clause later
+            print(output_path)
+            print("extracting Junit version")
+            junit_version, source_dir, test_source_dir = self.extract_junit_version( input_path, output_path )
+            print("Junit version: ", junit_version)
+
+            if not "test_file_path" in d:
+                if test_source_dir is not None and source_dir is not None:
+                    print(d["code_file_path"])
+
+                    d["test_file_path"] = d["code_file_path"].replace(source_dir.replace("/root/" , ""), test_source_dir.replace("/root/" , ""))
+                    d["test_file_path"] = d["test_file_path"].replace(".java", "Test.java")
+                    print(d["test_file_path"])
+                else:
+                    d["test_file_path"] = d["code_file_path"].replace(".java", "Test.java")
+
+            d["junit_version"] = junit_version #remove that later
+            save_dicts_list_to_json([d], ana_path)
+
+
+            return
+
+        else:
+            junit_version = d["junit_version"]
+
+        test_class_real_name = d["test_file_path"].split("/")[-1].split(".")[0]
+        test_class_unique_name = "THIS_IS_A_UNIQUE_NAME_Test"
+
+        # found in imports ?
+        junit_found = False
+
+        if not "test_package" in d:
+            print("no tests were extracted for this method")
+            d["test_package"] = d["package"]
+
+
+        else:
+            for imp in d["test_imports"]:
+                if "junit" in imp:
+                    junit_found = True
+                    break
+
+        if not junit_found:
+            if junit_version.startswith("3"):
+                d["test_imports"] = ["import junit.framework.*;\n"]
+            elif junit_version.startswith("4."):
+                d["test_imports"] = ["import org.junit.*;\n" , "import static org.junit.Assert.*;\n"]
+            else:
+                d["test_imports"] = ["import org.junit.jupiter.api.*;\n"]
+
+
+        print(f"Starting analysis of function: {d['signature']['name']}")
+        print("    Level 1")
+
+        if not "new_tests" in d:
+            print("generate new tests")
+            new_tests, response = self.generator.generate_tests(d, input_path, output_path)
+
+            d["new_tests"] = new_tests
+            d["new_tests_response"] = response
+
+        else:
+            print("new tests already generated")
+
+        print("execute new tests")
+
+        d["results"] = {}
+        d["results"]["(code, new_tests)"] = [[],[],[]]
+
+        for dt in d["new_tests"]:
+            print("testing property:", dt["property"])
+
+            dt["new_tests"] = dt["new_tests"].replace(test_class_real_name, test_class_unique_name)
+            d["intermediate_tests"] = dt["new_tests"]
+            d["test_file_path"] = d["test_file_path"].replace( test_class_real_name, test_class_unique_name )
+
+            res2 = list(self.executor.execute("code", "intermediate_tests", d, input_path, output_path))
+            dt["new_tests"] = dt["new_tests"].replace(test_class_unique_name, test_class_real_name)
+            d["test_file_path"] = d["test_file_path"].replace(test_class_unique_name,  test_class_real_name)
+
+            evaluated = self.evaluate(res2)
+
+            # this is the compilatio nerror loop.  turn back on if nedded by etiher making this a true or removingg the check.
+            compilerror = False
+            if compilerror:
+                for i in range(2):
+                    # repair step
+                    if evaluated == 0:
+                        with open( output_path + "/log.txt", "r") as f:
+                            exec_output = f.read()
+                        # If it errored we want to know the compilation error:
+
+                        matches = re.findall(r'\[ERROR\] COMPILATION ERROR :[\s\S]*?\[INFO\] -*\n(.*?)\[INFO\]', exec_output, re.DOTALL)
+
+                        if not matches:
+                            # No match (compilation error) found.
+                            with open( output_path + "/log.txt", "a") as f:
+                                f.write("No compilation error found\n")
+
+                        else:
+                            # Get the last occurrence
+                            comp_error = matches[-1].strip()
+                            comp_error = comp_error.replace( test_class_unique_name , test_class_real_name )
+
+                            new_tests , response = self.generator.repair_tests(d, input_path, output_path, comp_error, 'new_tests')
+
+                            d["new_tests"] = new_tests
+                            d["new_tests_repair_response"] = response
+
+                            print("execute repaired tests")
+
+                            d["new_tests"] = d["new_tests"].replace(test_class_real_name, test_class_unique_name)
+                            d["test_file_path"] = d["test_file_path"].replace(test_class_real_name, test_class_unique_name)
+                            res2 = list(self.executor.execute("code", "new_tests", d, input_path, output_path))
+                            d["new_tests"] = d["new_tests"].replace(test_class_unique_name, test_class_real_name)
+                            d["test_file_path"] = d["test_file_path"].replace(test_class_unique_name, test_class_real_name)
+
+                            d["results"]["(code, new_tests)"] = res2
+
+                            save_dicts_list_to_json([d], ana_path)
+
+                            evaluated = self.evaluate(d["results"]["(code, new_tests)"])
+
+            amount_generated_testclasses+=1
+
+
+            if evaluated == 0:
+                output = "Test for property: " + dt["property"] + " errored and will be ignored"
+                dt["new_tests"] = None   #delete test so that it is never executed again
+                d["results"]["(code, new_tests)"][2].append(dt["property"])
+                amount_errored_testclasses += 1
+                print(f"errored classes so far: {amount_errored_testclasses} out of {amount_generated_testclasses} generated")
+
+            elif evaluated == 1:
+                d["results"]["(code, new_tests)"][0].append(dt["property"])
+
+            else:
+                d["results"]["(code, new_tests)"][1].append(dt["property"])
+
+            save_dicts_list_to_json([d], ana_path)
+
+
+
+            # check if overall tests passed or failed or errored
+        evaluated = self.evaluate(d["results"]["(code, new_tests)"])
+
+        if evaluated >= 0:
+            output = "Negative"
+            output += ", error in layer 2: code, new_tests" if evaluated == 0 else ", pass in layer 2: code, new_tests"
+
+        else:
+            # generate new code  -----------------------------------------------------------------------------------------------
+
+
+
+            new_code, response = self.generator.generate_code(d, input_path, output_path)
+
+            d["new_code"] = new_code
+            d["new_code_response"] = response
+
+            print("execute new code")
+
+            d["new_tests"] = d["new_tests"].replace(test_class_real_name, test_class_unique_name)
+            d["test_file_path"] = d["test_file_path"].replace(test_class_real_name, test_class_unique_name)
+            res3 = list(self.executor.execute("new_code", "new_tests", d, input_path, output_path))
+            d["new_tests"] = d["new_tests"].replace(test_class_unique_name, test_class_real_name)
+            d["test_file_path"] = d["test_file_path"].replace(test_class_unique_name, test_class_real_name)
+
+            
+            d["results"]["(new_code, new_tests)"] = res3
+            save_dicts_list_to_json([d], ana_path)
+
+            # check if it errored
+            evaluated = self.evaluate(d["results"]["(new_code, new_tests)"])
+            if evaluated == 0:
+                with open(output_path + "/log.txt", "r") as f:
+                    exec_output = f.read()
+                # If it errored we want to know the compilation error:
+
+                matches = re.findall(r'\[ERROR\] COMPILATION ERROR :[\s\S]*?\[INFO\] -*\n(.*?)\[INFO\]', exec_output, re.DOTALL)
+
+                if not matches:
+                    # No match (compilation error) found.
+                    with open(output_path + "/log.txt", "a") as f:
+                        f.write("No compilation error found\n")
+
+                else:
+                    # Get the last occurrence
+                    comp_error = matches[-1].strip()
+                    comp_error = comp_error.replace( test_class_unique_name , test_class_real_name )
+                    
+                    new_code, response = self.generator.repair_code(d, input_path, output_path, comp_error, 'new_code')
+
+                    d["new_code"] = new_code
+                    d["new_code_repair_response"] = response
+
+                    print("execute repaired code")
+
+                    d["new_tests"] = d["new_tests"].replace(test_class_real_name, test_class_unique_name)
+                    d["test_file_path"] = d["test_file_path"].replace(test_class_real_name, test_class_unique_name)
+                    res3 = list(self.executor.execute("new_code", "new_tests", d, input_path, output_path))
+                    d["new_tests"] = d["new_tests"].replace(test_class_unique_name, test_class_real_name)
+                    d["test_file_path"] = d["test_file_path"].replace(test_class_unique_name, test_class_real_name)
+
+
+                    
+                    d["results"]["(new_code, new_tests)"] = res3
+                    save_dicts_list_to_json([d], ana_path)
+
+                    evaluated = self.evaluate(d["results"]["(new_code, new_tests)"])
+
+            if evaluated <= 0:
+                output += "Negative"
+                output += ", error in layer 3: new_code, new_tests" if evaluated == 0 else ", fail in layer 3: new_code, new_tests"
+
+            else:
+                output += "Positive"
+
+        with open("result.txt", "w") as f:
+            f.write(output)
+            print("result:" , output)
+
+
+    def evaluate(self, res):
+        if res[0] == [] and res[1] == []:
+            if self.debug >= 1:
+                print("        Error")
+            # error
+            return 0
+        elif res[1] == [] and res[2] == []:
+            if self.debug >= 1:
+                print("        Passed")
+            # if no errors or failures  then passed
+            return 1
+        else:
+            if self.debug >= 1:
+                print("        Failed")
+            # failed
+            return -1
---
+++ src/cascade/generation/test/MultiStepJavaTestGenerator.py
@@ -0,0 +1,146 @@
+import ast
+import copy
+import json
+import os
+import re
+import subprocess
+from platform import system
+
+import tiktoken
+
+from cascade.generation.test.GPT4JavaTestGenerator import GPT4JavaTestGenerator
+from cascade.generation.executor.OpenAIChatCompletionExecutor import OpenAIChatCompletionExecutor
+from cascade.utils.JavaUtils import build_context, check_syntax, repair_helper_functions, get_repair_helper_functions, build_signature
+
+
+class MultiStageJavaTestGenerator(GPT4JavaTestGenerator):
+    def __init__(self, max_attempts=1, max_tokens=16000, temperature=0, delay=3, max_prompt_tokens=5000, model="gpt-4o-mini-2024-07-18", freq_penalty=0.0, dummy=False, ask_for_imports=False, import_prompt_finisher="Reply with the missing imports, leave out those you don't know the correct package of."):
+        super().__init__()
+        self.ask_for_imports = ask_for_imports
+        self.model = model
+        self.import_prompt_finisher = import_prompt_finisher
+        self.max_prompt_tokens = max_prompt_tokens
+        self.prompt_executor = OpenAIChatCompletionExecutor(max_attempts=max_attempts, model=model, max_tokens=max_tokens, temperature=temperature,
+                                                            delay=delay, freq_penalty=freq_penalty, dummy=dummy)
+
+        self.is_three = False
+
+
+    def build_prompt(self, context):
+        enc = tiktoken.encoding_for_model(self.model)
+
+        testframework = ""
+        if "junit_version" in context:
+            version = context["junit_version"]
+            testframework = " Use JUnit version " + (version if version[0].isdigit() else "5") + "."
+
+        par = context['signature']['params']
+        params = ", ".join(par) if len(par) > 1 else (par[0] if par else "")
+
+        # system_prompt = f"Write Java tests for the function {context['signature']['name']}. Follow its documentation as closely as possible."
+
+        # The method is not implemented yet so you will be using only its documentation as the ground truth of the expected behavior.
+        # Focus on creating tests that cover edge cases, boundary conditions, and all documented behaviors, including thrown exceptions
+
+        system_prompt = f"You are a Java developer. You will generate unit tests for a specific method in a provided class.{testframework} You can import anything from the project, but no third party libraries. Handle exceptions properly, and ensure method signatures and calls are correct. The code should compile without errors."
+
+        # This is for test driven development so the tests should be designed to fail if the later implementation does not conform to the documentation.
+        c1 = f"The function under test is `{context['signature']['name']}({params})`\n\n Focus on testing: {context["tested_property"]["property"]}. Especially on the following Things: {'\n'.join(context["tested_property"]["tests"])}\nHere is the class containing the function:\n\n```java\n"
+        c2 = "; // this is the function to be tested\n\n}\n```\n"
+        code = c1 + build_context(context, doc=True) + c2
+
+        # test_header = "\n\n// TESTS:\n\n" + self.build_tests(context, primer=f"\n    // write tests for {context['signature']['name']} here. Take the Documentation as literal as possible.\n")
+
+        test_header = f"\nNow write several tests regarding: {context["tested_property"]["property"]} for the function `{context['signature']['name']}({params})` using the test class skeleton below. Everything that you use has to be added to the imports. Every object you use has to be properly instantiated. Handle any checked exceptions (use try-catch or throws), do not forget type parameters. Match method signatures exactly when overriding or implementing methods. Adhere to the documentation as close as possible when writing the tests and try to test for the following things: \n{'\n'.join(context["tested_property"]["tests"])} \nAs a reminder, the documentation for the function is:\n\n```java\n{context['doc']}\n```\n\n Test class:\n"
+        test_header = test_header + "\n```java\n" + self.build_tests(context, primer=f"\n    // write tests for {context['signature']['name']} here." + "\n```")
+
+        # add something like?  :    Include necessary constructors if extending a class that requires them.
+
+        prompt = code + test_header
+
+        if len(enc.encode(prompt)) > self.max_prompt_tokens:
+            code = c1 + build_context(context, doc=True, no_fields=True) + c2
+            prompt = code + test_header
+
+        if len(enc.encode(prompt)) > self.max_prompt_tokens:
+            code = c1 + build_context(context, doc=True, no_fields=True, no_constructors=True) + c2
+            prompt = code + test_header
+
+        if len(enc.encode(prompt)) > self.max_prompt_tokens:
+            code = c1 + build_context(context, doc=True, no_fields=True, no_constructors=True,
+                                      no_other_method_docs=True) + c2
+            prompt = code + test_header
+
+        if len(enc.encode(prompt)) > self.max_prompt_tokens:
+            code = c1 + build_context(context, doc=True, no_fields=True, no_constructors=True,
+                                      no_other_method_docs=True,
+                                      no_other_methods=True) + c2
+            prompt = code + test_header
+
+        if len(enc.encode(prompt)) > self.max_prompt_tokens:
+            return []
+
+        promptlist = []
+        promptlist.append({"role": "system", "content": system_prompt})
+        promptlist.append({"role": "user", "content": prompt})
+
+        return promptlist
+
+
+    def generate(self, context, input_path, output_path):
+        # first given the method documentation and signature, we want to extract possible testscases or properties.
+        print("generation Phase 1")
+        prompt_stage1 = [
+        {"role": "system", "content": "You are an Expert Java Developer and Requirements Engineer. You will be given a method signature and its documentation. Your task is to extract testable properties from the documentation."},
+        {"role": "user", "content": f"Give me properties that should be tested for the method:\n ```java\n{build_signature(context, doc=True)}\n```\nMake sure the entire functionality as described in the documentation is considered and covered with these tests."}
+        ]
+
+        response_stage1 = self.prompt_executor.execute(prompt_stage1).model_dump()
+
+        # some type of check if it is ok???
+        prompt_stage1.append(response_stage1["choices"][0]["message"])
+
+        # next we aim to convert this list into a usable format and extract the to tested properties
+
+        prompt_stage1.append({"role": "user", "content": f"Now turn this into a JSON array. Where each entry has \"property\" (the tested property) and \"tests\" a list of things that should be tested to verify the property" })
+
+        response_stage1_part2 = self.prompt_executor.execute(prompt_stage1).model_dump()
+
+        answer_text = response_stage1_part2["choices"][0]["message"]["content"]
+
+
+        print(answer_text)
+        json_blocks = re.findall(r"```json(.*?)\n\s*```", answer_text, flags=re.DOTALL)
+
+        if json_blocks:
+            try:
+                tests_list = ast.literal_eval(json_blocks[0].strip())
+            except Exception as e:
+                print("Failure: Could not parse json list because: ", e)
+        else:
+            print("error extracting test list block")
+            return # TODO some error handling
+
+        print(f"Got {len(tests_list)} potential test properties: {tests_list}")
+        print("generation Phase 2")
+
+        final_response = []
+        final_new_tests = []
+
+        for test in tests_list[0:3]:
+            # now generate a test class for each property
+            context["tested_property"] = test
+
+            print("generate tests for:", test["property"])
+            prompt = self.build_prompt(context)
+
+            response = self.prompt_executor.execute(prompt).model_dump()
+            new_tests = self.extract_tests(response["choices"][0]["message"]["content"], context, response, output_path)
+
+            prompt.append({"role": "assistant", "content": f"´´´java\n{new_tests}\n```"})
+
+            final_response.append({"property" : test["property"], "prompt" : prompt, "response" : response} )
+
+            final_new_tests.append({ "new_tests" : new_tests , "property" : test["property"]})
+
+        return final_new_tests, final_response
