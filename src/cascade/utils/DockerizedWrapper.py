0f393d4b245171a8c8e94e8feffcad17718692b6
TobiasKiecker <tobias.kiecker@web.de>
2026-02-12 18:35:58 +0100

    reverted lasst change

--- src/cascade/utils/DockerizedWrapper.py
+++ src/cascade/utils/DockerizedWrapper.py
@@ -103,12 +103,10 @@ class DockerizedWrapper:
         try:
             if images:
                 self.remove_image(dock_context)
-        except Exception as e:
-            print(f"Couldn't remove image because of Exception: {e}")
-        finally:
             container = self.set_up(dock_context)
             exit_code = self.run(container, dock_context, output_path)
             container.commit(dock_context["new_image"])
+            finally:
             if container:
                 self.kill(container)
             return exit_code
