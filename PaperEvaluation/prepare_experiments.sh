0d7e720deeda56df9873ac7e3abfeefad2176368
Caspar Klein <caspar.m.klein@gmail.com>
2026-05-06 18:39:33 +0200

    fix install-commands using old venv paths in prepare experiments script

--- PaperEvaluation/prepare_experiments.sh
+++ PaperEvaluation/prepare_experiments.sh
@@ -5,20 +5,20 @@ python3 -m venv venv-cascade
 
 # install it:
 
-./cascade.venv/bin/pip install ..
+./venv-cascade/bin/pip install ..
 
 
 
 echo "build DocChecker venv"
 
 python3 -m venv venv-DocChecker
-./envDocChecker/bin/pip install torch transformers gdown codetext tree_sitter_languages tree_sitter==0.20.4
+./venv-DocChecker/bin/pip install torch transformers gdown codetext tree_sitter_languages tree_sitter==0.20.4
 
    
 echo "build C4RLLaMA venv"
 
 python3 -m venv venv-C4RLLaMA
-./envC4RLLaMA/bin/pip install -r ./drivers/C4RLLaMA/requirements.txt
+./venv-C4RLLaMA/bin/pip install -r ./drivers/C4RLLaMA/requirements.txt
 
 echo "you have to download the retrained DocChecker model https://figshare.com/s/981c2fbe830b905b01a9 and put it in /drivers/DocChecker/pretrained_model"
 echo "you have to download the retrained C4RLLaMA weights https://figshare.com/s/812541da6a1f33025f69 and put them in /drivers/C4RLLaMA/weights"
